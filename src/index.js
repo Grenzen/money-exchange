b154f54e59e6e58827a3a788e7cc3dcf822b8eb7
Rita Mat <grenzentur@rambler.ru>
2019-02-21 17:11:14 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ module.exports = function makeExchange(currency) {
     if (currency > 10000) {
         return { error: "You are rich, my friend! We don't have so much coins for exchange" };
     } else {
-        var nominals = [H50, Q25, D10, N5, P1];
+        var nominals = ['H50', 'Q25', 'D10', 'N5', 'P1'];
         var nominalLen = nominals.length;
+        console.log(typeof nominals, nominalLen);
 
         for (var i = 0; i < nominalLen; i++) {
 
 	        while (currency >= 0 && currency >= +nominals[i].slice(1)) {
-		        exchange.nominals[i].substring(0,1) = parseInt( currency/+(nominals[i].slice(1)) );
-	            currency = currency % +(nominals[i].slice(1));
+                //var key = nominals[i].substring(0,1);
+		        exchange[nominals[i].substring(0,1)] = parseInt( currency/+nominals[i].slice(1) );
+	            currency = currency % +nominals[i].slice(1);
             }
         }
+        return exchange;
     }
-	return exchange;
 }
